48cf74ae1c1dfd4ce9eb8ff8a799e4a85a393169
Steve ChoCho <cyk3233@gmail.com>
2022-06-24 15:33:25 +0900

    Terraform AWS Module VPC

--- .history/eip_20220624114740.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "nat_eip" {
-    # count = length(var.azs)
-    count = "${var.enable_internet_gateway == "true" && var.enable_nat_gateway == "true" ? length(var.azs) : 0 }"
-
-    vpc = true
-
-    tags = merge(var.tags, tomap({"Name" = format("%s-nat-%s-eip, ${var.prefix}, ${substr(var.azs, -2. 2)}")}))
-}
