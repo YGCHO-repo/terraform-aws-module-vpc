7008a88b114619009ca9efdaba20a740a6b5b219
Steve ChoCho <cyk3233@gmail.com>
2022-06-24 11:50:02 +0900

    VPC Module

---
+++ subnet.tf
@@ -0,0 +1,24 @@
+# Dynamic subnet
+resource "aws_subnet" "this" {
+    vpc_id = aws_vpc.this.id
+
+    for_each = { for i in local.all_subnets : i.cidr => i}
+
+    cidr_block = each.key
+    availability_zone = var.azs[index(var.subnets[each.value.name].cidr, each.key)]
+
+    tags = merge(var.tags,
+        tomap({
+            Name = format(
+                "%s-%s-%s-%s-%s-subnet",
+                var.prefix,
+                var.vpc_name,
+                var.azs[index(var.subnets[each.value.name].cidr, each.key)],
+                var.subnets[each.value.name].ipv4_type,
+                each.value.name
+            )
+        }),
+        var.subnets[each.value.name].ipv4_type == "private" ? {"Tier" = "private"} : {},
+        var.subnets[each.value.name].ipv4_type == "public" ? {"Tier" = "public"} : {}
+    )
+}
