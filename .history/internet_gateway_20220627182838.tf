005cef6cdfe141c2ef8faf293461c249502b48d3
Steve ChoCho <cyk3233@gmail.com>
2022-12-29 01:37:49 +0900

    Terraform AWS Module VPC

--- .history/internet_gateway_20220627182838.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "this" {
-    vpc_id = var.enable_internet_gateway == "ture" ? aws_vpc.this.id : ""
-    
-    tags = merge(var.tags, tomap({Name = format("%s-%s-igw", var.prefix, var.vpc_name)})) 
-}
-
